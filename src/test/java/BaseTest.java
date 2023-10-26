fc569097dc442fde606755ba0a1645724c813f46
kinjl19 <142246931+kinjl19@users.noreply.github.com>
2023-10-25 23:26:56 -0400

    8.0

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,59 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+import java.time.Duration;
 
+public class BaseTest {
+    public WebDriver driver = null;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    public static void openBrowser(String siteUrl) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(siteUrl);
+
+        Assert.assertEquals(driver.getCurrentUrl(), siteUrl);
+    }
+
+    public void loginSite(String useName, String pswd){
+
+        WebElement emUser = driver.findElement(By.cssSelector("[type='email']"));
+        emUser.sendKeys(useName);
+
+        WebElement emPswd = driver.findElement(By.cssSelector("[type='password']"));
+        emPswd.sendKeys(pswd);
+
+        WebElement emLogin = driver.findElement(By.cssSelector("[type='submit']"));
+        emLogin.click();
+    }
+
+    public void klSearch(String songName){
+        WebElement klSearch = driver.findElement(By.cssSelector("[type='search']"));
+        klSearch.sendKeys("midnight");
+
+        WebElement vAll = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        vAll.click();
+
+        WebElement fList = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr"));
+        fList.click();
+
+        WebElement AddTo = driver.findElement(By.cssSelector("[class='btn-add-to']"));
+        AddTo.click();
+
+        WebElement selPlaylist = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/div/section[1]/ul/li[12]"));
+        selPlaylist.click();
     }
 }
--- src/test/java/homework17.java
+++ src/test/java/homework17.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.ser.Serializers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,43 +13,11 @@ public class homework17 extends BaseTest {
     public void addSongToPlaylist() {
 
 //      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-
-        WebElement emUser = driver.findElement(By.cssSelector("[type='email']"));
-        emUser.sendKeys("kinjal.borad@testpro.io");
-
-        WebElement emPswd = driver.findElement(By.cssSelector("[type='password']"));
-        emPswd.sendKeys("Ganesh#123");
-
-        WebElement emLogin = driver.findElement(By.cssSelector("[type='submit']"));
-        emLogin.click();
-
-        WebElement klSearch = driver.findElement(By.cssSelector("[type='search']"));
-        klSearch.sendKeys("midnight");
-
-        WebElement vAll = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
-        vAll.click();
-
-        WebElement fList = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr"));
-        fList.click();
-
-        WebElement AddTo = driver.findElement(By.cssSelector("[class='btn-add-to']"));
-        AddTo.click();
-
-        WebElement selPlaylist = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/div/section[1]/ul/li[12]"));
-        selPlaylist.click();
-
-        String adConfirm = "Added 1 song into kinjal";
-        //Assert.assertEquals(driver.findElement(By.cssSelector("[class='alertify-logs top right']")).getText(), adConfirm);
+        openBrowser("https://qa.koel.app/");
+        loginSite("kinjal.borad@testpro.io", "Ganesh#123");
+        klSearch("midnight");
+        String adConfirm = "Added 1 song into \" kinjal \"";
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.success.show")).getText(), adConfirm);
         //Test Comment
         driver.quit();
 
