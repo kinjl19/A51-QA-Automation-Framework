e46ad58e50abf02249d011096b27a6ddd0db1cd1
kinjl19 <142246931+kinjl19@users.noreply.github.com>
2023-10-29 22:29:54 -0400

    HW18_1

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Homework18 extends BaseTest {
+
+    @Test
+    public void playSong(){
+
+    }
+}
